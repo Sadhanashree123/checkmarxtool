39d4ae8cf0c244e94a0ba2d81444c7f09814d809
Jeeva Yovan E <127444009+Ava-JeevaYovanE@users.noreply.github.com>
2023-11-15 21:23:21 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -11,86 +11,68 @@ resource "aws_ecr_repository" "ecr_repo" {
  }
 }
 module "aws_vpc" {
+  source          = "github.com/erozedguy/AWS-VPC-terraform-module.git?ref=v0.1.0"
   networking      = var.networking
   security_groups = var.security_groups
 }
-resource "aws_iam_role" "EKSClusterRole" {
-  name = "EKSClusterRole"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
-resource "aws_iam_role" "NodeGroupRole" {
-  name = "EKSNodeGroupRole"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
 
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
-resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.EKSClusterRole.name
-}
-resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.NodeGroupRole.name
-}
-resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.NodeGroupRole.name
-}
+# EKS Cluster
 resource "aws_eks_cluster" "eks-cluster" {
-  name     = "eks-cluster"
+  name     = var.cluster_config.name
   role_arn = aws_iam_role.EKSClusterRole.arn
-  version  = "1.21"
+  version  = var.cluster_config.version
 
   vpc_config {
-    subnet_ids          = flatten([ module.aws_vpc.public_subnets_id, module.aws_vpc.private_subnets_id ])
-    security_group_ids  = flatten(module.aws_vpc.security_groups_id)
+    subnet_ids         = flatten([module.aws_vpc.public_subnets_id, module.aws_vpc.private_subnets_id])
+    security_group_ids = flatten(module.aws_vpc.security_groups_id)
   }
 
   depends_on = [
     aws_iam_role_policy_attachment.AmazonEKSClusterPolicy
   ]
+
 }
+
+# NODE GROUP
 resource "aws_eks_node_group" "node-ec2" {
+  for_each        = { for node_group in var.node_groups : node_group.name => node_group }
   cluster_name    = aws_eks_cluster.eks-cluster.name
-  node_group_name = "t3_micro-node_group"
+  node_group_name = each.value.name
   node_role_arn   = aws_iam_role.NodeGroupRole.arn
-  subnet_ids      = flatten( module.aws_vpc.private_subnets_id )
+  subnet_ids      = flatten(module.aws_vpc.private_subnets_id)
 
   scaling_config {
-    desired_size = 2
-    max_size     = 3
-    min_size     = 1
+    desired_size = try(each.value.scaling_config.desired_size, 2)
+    max_size     = try(each.value.scaling_config.max_size, 3)
+    min_size     = try(each.value.scaling_config.min_size, 1)
   }
 
-  ami_type       = "AL2_x86_64"
-  instance_types = ["t3.micro"]
-  capacity_type  = "ON_DEMAND"
-  disk_size      = 20
+  update_config {
+    max_unavailable = try(each.value.update_config.max_unavailable, 1)
+  }
+
+  ami_type       = each.value.ami_type
+  instance_types = each.value.instance_types
+  capacity_type  = each.value.capacity_type
+  disk_size      = each.value.disk_size
 
   depends_on = [
     aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
     aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy
   ]
+}
+
+resource "aws_eks_addon" "addons" {
+  for_each          = { for addon in var.addons : addon.name => addon }
+  cluster_name      = aws_eks_cluster.eks-cluster.id
+  addon_name        = each.value.name
+  addon_version     = each.value.version
+  resolve_conflicts = "OVERWRITE"
+}
+
+resource "aws_iam_openid_connect_provider" "default" {
+  url             = "https://${local.oidc}"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da2b0ab7280"]
 }
