81d87bc21fa18efabfd0f2dc428a9107f7ff7ae5
Jeeva Yovan E <127444009+Ava-JeevaYovanE@users.noreply.github.com>
2023-11-15 20:40:49 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -88,20 +88,6 @@ vpc_id = module.vpc.vpc_id
     workers_group_defaults = {
         root_volume_type = "gp2"
     }
-workers_group = [
-        {
-            name = "Worker-Group-1"
-            instance_type = "t2.micro"
-            asg_desired_capacity = 2
-            additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
-        },
-        {
-            name = "Worker-Group-2"
-            instance_type = "t2.micro"
-            asg_desired_capacity = 1
-            additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-        },
-    ]
 }
 data "aws_eks_cluster" "cluster" {
     name = module.eks.cluster_id
